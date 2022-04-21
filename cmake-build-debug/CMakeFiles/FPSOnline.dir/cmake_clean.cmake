30ad3c77d98f3418e72225e4e93c615ca2b0951f
Cppshizoid <alex.krivoschecov@gmail.com>
2022-04-21 18:36:36 +0300

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/FPSOnline.dir/cmake_clean.cmake
@@ -0,0 +1,36 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FPSOnline.dir/Bonus.cpp.obj"
+  "CMakeFiles/FPSOnline.dir/Button.cpp.obj"
+  "CMakeFiles/FPSOnline.dir/Camera.cpp.obj"
+  "CMakeFiles/FPSOnline.dir/Circle2D.cpp.obj"
+  "CMakeFiles/FPSOnline.dir/ClientUDP.cpp.obj"
+  "CMakeFiles/FPSOnline.dir/Enemy.cpp.obj"
+  "CMakeFiles/FPSOnline.dir/GeneticAlgorithm.cpp.obj"
+  "CMakeFiles/FPSOnline.dir/Menu.cpp.obj"
+  "CMakeFiles/FPSOnline.dir/MsgType.cpp.obj"
+  "CMakeFiles/FPSOnline.dir/NeuralNetwork.cpp.obj"
+  "CMakeFiles/FPSOnline.dir/Object2D.cpp.obj"
+  "CMakeFiles/FPSOnline.dir/OptimizedBots.cpp.obj"
+  "CMakeFiles/FPSOnline.dir/Player.cpp.obj"
+  "CMakeFiles/FPSOnline.dir/Point2D.cpp.obj"
+  "CMakeFiles/FPSOnline.dir/Point3D.cpp.obj"
+  "CMakeFiles/FPSOnline.dir/Poligon2D.cpp.obj"
+  "CMakeFiles/FPSOnline.dir/ReliableMsg.cpp.obj"
+  "CMakeFiles/FPSOnline.dir/ResourceManager.cpp.obj"
+  "CMakeFiles/FPSOnline.dir/ServerUDP.cpp.obj"
+  "CMakeFiles/FPSOnline.dir/Time.cpp.obj"
+  "CMakeFiles/FPSOnline.dir/UDPConnection.cpp.obj"
+  "CMakeFiles/FPSOnline.dir/UDPSocket.cpp.obj"
+  "CMakeFiles/FPSOnline.dir/Weapon.cpp.obj"
+  "CMakeFiles/FPSOnline.dir/World.cpp.obj"
+  "CMakeFiles/FPSOnline.dir/main.cpp.obj"
+  "FPSOnline.exe"
+  "FPSOnline.exe.manifest"
+  "FPSOnline.pdb"
+  "libFPSOnline.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FPSOnline.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
